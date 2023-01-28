b290902717f2cd534c488d75223bf455348b4359
XDCoolMan <dcorniello.business@gmail.com>
2023-01-28 14:58:58 -0500

    Initial Commit

---
+++ InItToWinIt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+ * This program will print out the ticket for an entry to an obscure
+ * music competition including the competition name, title
+ * who popularized it, and the time for the song.
+ * 
+ * Electronic Signature: Derek Corniello
+ */
+
+int main() 
+{
+	std::cout << "Air Guitar\n";
+	std::cout << "Stairway to Heavan" << std::endl;
+	//written by: James Patrick Page, Jimmy Page, and Robert Plant
+	std::cout << "As performed by: Led Zeppelin\n";
+	std::cout << "Time: 8:03" << std::endl;
+	return 0;
+}
---
+++ MenuMkr.cpp
@@ -0,0 +1,15 @@
+/*
+ * Lab00 - MenuMkr - corniedj.cpp : This file contains the 'main' function. Program execution begins and ends there.
+ * Description: Console outputs the Prix Fixe of a restaraunt's menu
+ */ 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Coding Cafe\n"; 
+    cout << "Appetizer: Avocado Toast" << endl; //cost to prepare: $0.75
+    cout << "Entree: Grilled Cheese" << endl; //cost to prepare: $2.70
+    cout << "Dessert: Cinnamon Rolls\n"; //cost to prepare: $1.45
+}
